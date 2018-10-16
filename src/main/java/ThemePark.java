84b993160e1dbe144c4aaf5e07a6a24edf250e12
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-16 16:00:56 +0100

    themepark created

---
+++ src/main/java/ThemePark.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+public class ThemePark {
+
+    private String name;
+
+    private CandyFlossStall floss;
+    private Dodgems dodgems;
+    private IceCreamStall icecream;
+    private Park park;
+    private Playground playground;
+    private Rollercoaster rollercoaster;
+    private TobaccoStall tobacco;
+    private ArrayList<Visitor> visitors;
+    public ThemePark(String name, CandyFlossStall floss, Dodgems dodgems,IceCreamStall icecream,Park park,Playground playground,Rollercoaster rollercoaster,TobaccoStall tobacco) {
+        this.name = name;
+        this.floss = floss;
+        this.dodgems = dodgems;
+        this.icecream = icecream;
+        this.park = park;
+        this.playground = playground;
+        this.rollercoaster = rollercoaster;
+        this.tobacco = tobacco;
+        this.visitors = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CandyFlossStall getFloss() {
+        return floss;
+    }
+
+    public Dodgems getDodgems() {
+        return dodgems;
+    }
+
+    public IceCreamStall getIcecream() {
+        return icecream;
+    }
+
+    public Park getPark() {
+        return park;
+    }
+
+    public Playground getPlayground() {
+        return playground;
+    }
+
+    public Rollercoaster getRollercoaster() {
+        return rollercoaster;
+    }
+
+    public TobaccoStall getTobacco() {
+        return tobacco;
+    }
+
+    public ArrayList<Visitor> getVisitors() {
+        return visitors;
+    }
+
+    public void addVisitor(Visitor visitor){
+        visitors.add(visitor);
+    }
+
+    public void removeVisitor(Visitor visitor){
+        visitors.remove(visitor);
+    }
+
+    public void parkShut(){
+        visitors.clear();
+    }
+}
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,112 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+
+    ThemePark themePark;
+    CandyFlossStall floss;
+    Dodgems dodgems;
+    IceCreamStall icecream;
+    TobaccoStall tobacco;
+    Rollercoaster roller;
+    Playground playground;
+    Park park;
+
+    Visitor anna;
+    Visitor bob;
+    Visitor charlie;
+    Visitor denise;
+    Visitor emily;
+
+    @Before
+    public void before(){
+        dodgems = new Dodgems("Dodgem This");
+        floss = new CandyFlossStall("Mental Floss", "Jenny", "C2");
+        icecream = new IceCreamStall("Ice To Meet You", "Ben", "B12");
+        tobacco = new TobaccoStall("Smoke on the Water", "Joe", "A24");
+        roller = new Rollercoaster("Keep on Rollin'");
+        playground = new Playground("The PlaySpace");
+        park = new Park("The Park");
+
+        themePark = new ThemePark("ThemePark",floss,dodgems,icecream,park,playground,roller,tobacco);
+
+        anna = new Visitor(12, 110, 5.00);
+        bob = new Visitor(20, 180, 50.00);
+        charlie = new Visitor(18, 150, 60.00);
+        denise = new Visitor(16, 135, 40.00);
+        emily = new Visitor(28, 160, 100.00);
+
+
+        themePark.addVisitor(anna);
+        themePark.addVisitor(bob);
+        themePark.addVisitor(charlie);
+        themePark.addVisitor(denise);
+        themePark.addVisitor(emily);
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("ThemePark", themePark.getName());
+    }
+
+    @Test
+    public void getFloss(){
+        assertEquals(floss, themePark.getFloss());
+    }
+
+    @Test
+    public void getDodgems(){
+        assertEquals(dodgems,themePark.getDodgems());
+    }
+
+    @Test
+    public void getIceCream(){
+        assertEquals(icecream, themePark.getIcecream());
+    }
+
+    @Test
+    public void getPark() {
+        assertEquals(park, themePark.getPark());
+    }
+
+    @Test
+    public void getPlayground(){
+        assertEquals(playground, themePark.getPlayground());
+    }
+
+    @Test
+    public void getRollercoaster(){
+        assertEquals(roller, themePark.getRollercoaster());
+    }
+
+    @Test
+    public void getTobacco(){
+        assertEquals(tobacco, themePark.getTobacco());
+    }
+
+    @Test
+    public void getVisitorArray(){
+        assertEquals(5, themePark.getVisitors().size());
+    }
+
+    @Test
+    public void addVisitor(){
+        themePark.addVisitor(anna);
+        assertEquals(6, themePark.getVisitors().size());
+    }
+
+    @Test
+    public void removeVisitor(){
+        themePark.removeVisitor(anna);
+        assertEquals(4, themePark.getVisitors().size());
+    }
+
+    @Test
+    public void parkShut(){
+        themePark.parkShut();
+        assertEquals(0, themePark.getVisitors().size());
+    }
+}
+
