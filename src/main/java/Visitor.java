8dfedeece5462ac6c8dfe7b74a83f1b2be6cf38a
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-16 15:13:30 +0100

    Initial Commit

---
+++ src/main/java/Visitor.java
@@ -0,0 +1,11 @@
+public class Visitor {
+    private int age;
+    private int height;
+    private double money;
+
+    public Visitor(int age, int height, double money){
+        this.age = age;
+        this.height = height;
+        this.money = money;
+    }
+}
