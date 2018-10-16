3c116f587b85834138508b4a0fc0ae7b74c008fb
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-16 15:20:34 +0100

    abstract classes init

---
+++ src/main/java/Stall.java
@@ -0,0 +1,26 @@
+public abstract class Stall {
+
+    private String name;
+    private String owner;
+    private String parking;
+
+    public Stall(String name, String owner, String parking){
+        this.name = name;
+        this.owner = owner;
+        this.parking = parking;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getParking() {
+        return parking;
+    }
+
+
+}
