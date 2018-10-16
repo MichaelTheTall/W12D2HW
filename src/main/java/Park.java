c2ae27af434e578987119e286d9a5096dfbc4bcd
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-16 16:17:10 +0100

    park agecheck

--- src/main/java/Park.java
+++ src/main/java/Park.java
@@ -1,7 +1,12 @@
-public class Park extends Attraction {
+public class Park extends Attraction implements ISecurity {
 
     public Park(String name) {
         super(name);
     }
 
+    public boolean isAllowedTo(Visitor visitor){
+        if (visitor.getAge() > 15) {
+            return false;
+        } else return true;
+    }
 }
--- src/test/java/ParkTest.java
+++ src/test/java/ParkTest.java
@@ -6,14 +6,29 @@ import static org.junit.Assert.assertEquals;
 public class ParkTest {
 
     Park park;
+    Visitor anna;
+    Visitor bob;
 
     @Before
     public void before(){
         park = new Park("The Park");
+        anna = new Visitor("Anna",12, 110, 5.00);
+        bob = new Visitor("bob",20, 180, 50.00);
+
     }
 
     @Test
     public void getName(){
         assertEquals("The Park", park.getName());
+    }
+
+    @Test
+    public void ageCheckPass(){
+        assertEquals(true, park.isAllowedTo(anna));
+    }
+
+    @Test
+    public void ageCheckFail(){
+        assertEquals(false, park.isAllowedTo(bob));
     }
 }
